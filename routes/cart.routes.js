330d0f48280af620c663619c0a8dddb560b3b562
EazyErik <erik_busch@gmx.de>
2024-03-13 09:16:33 +0100

    add functionality for adding and deleting cart items

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -15,26 +15,49 @@ router.get("/cart", isAuthenticated, (req, res, next) => {
 });
 
 router.post("/cart", isAuthenticated, (req, res, next) => {
-  const { product, user } = req.body;
+  const { product } = req.body;
   CartItem.create({
     product,
-    user,
+    user: req.payload._id,
   })
     .then((createdCart) => {
+      console.log("createdCart", createdCart);
       res.status(201).json(createdCart);
     })
     .catch((error) => next(error));
 });
 
+// router.put("/cart/:productId", isAuthenticated, (req, res, next) => {
+//   const { productId } = req.params;
+//   const { product, user } = req.body;
+//   CartItem.findByIdAndUpdate(
+//     productId,{quantity:}
+
+//     { new: true }
+//   );
+// });
+
 router.delete("/cart/:productId", isAuthenticated, (req, res, next) => {
-  CartItem.findByIdAndDelete(req.params.productId)
+  const { productId } = req.params;
+  console.log(productId);
+  CartItem.deleteMany({ product: req.params.productId })
     .then(() => {
       res.sendStatus(204);
     })
     .catch((error) => next(error));
 });
 
-router.delete("/cart/", isAuthenticated, (req, res, next) => {
+router.delete("/cart/reduceQuantity/:id", isAuthenticated, (req, res, next) => {
+  const id = req.params.id;
+  console.log(id);
+  CartItem.findByIdAndDelete(id)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((error) => next(error));
+});
+
+router.delete("/cart", isAuthenticated, (req, res, next) => {
   console.log("user", req.payload);
   CartItem.deleteMany({ user: req.payload._id })
     .then(() => {
