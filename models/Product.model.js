367ef4fe89c03c97dad007ffe54f8798ca28bced
Marianna Di Vito <maridivi89@gmail.com>
2024-03-14 14:15:19 +0000

    Provide default values for product model

--- models/Product.model.js
+++ models/Product.model.js
@@ -4,10 +4,23 @@ const productSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
-  image: { type: String, default: "" },
+  image: {
+    type: String,
+    default:
+      "https://nayemdevs.com/wp-content/uploads/2020/03/default-product-image.png",
+  },
   category: {
     type: String,
-    enum: ["tech", "clothes", "furniture", "collectibles", "books", "vehicles"],
+    enum: [
+      "tech",
+      "clothes",
+      "furniture",
+      "collectibles",
+      "books",
+      "vehicles",
+      "other",
+    ],
+    default: "other",
   },
   createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
