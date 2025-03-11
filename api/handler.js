e1a0a81d284067460a62e5e23fae1e819c13958f
EazyErik <erik_busch@gmx.de>
2025-03-11 13:20:13 +0100

    Added debug route for Vercel

--- api/handler.js
+++ api/handler.js
@@ -1,4 +1,8 @@
 const app = require("../app"); // Importiere dein Express-App-Setup
 const serverless = require("serverless-http");
 
+app.get("/debug", (req, res) => {
+  res.json({ message: "Express on Vercel is working!" });
+});
+
 module.exports = serverless(app);
